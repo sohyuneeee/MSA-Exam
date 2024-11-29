caf3d350c5e50b1b7ec3dbfe7bba7539e2956220
sohyuneeee <sohyuney@naver.com>
2024-11-29 22:38:22 +0900

    feat: gateway 예외 처리 관련 클래스 생성

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.sparta.msa_exam.gateway.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    COMMON_INVALID_PARAMETER("잘못된 파라미터입니다.", BAD_REQUEST),
+    COMMON_SERVER_ERROR("서버에서 에러가 발생하였습니다.", INTERNAL_SERVER_ERROR),
+
+    AUTH_AUTHENTICATION_FAILED("인증에 실패하셨습니다.", UNAUTHORIZED),
+    AUTH_AUTHORIZATION_FAILED("권한이 없습니다.", FORBIDDEN),
+    AUTH_JWT_CLAIMS_EMPTY("JWT claims 문자열이 비어 있습니다.", UNAUTHORIZED),
+    AUTH_JWT_EXPIRED("만료된 토큰입니다.", UNAUTHORIZED),
+    AUTH_JWT_INVALID("잘못된 토큰입니다.", UNAUTHORIZED),
+    AUTH_JWT_UNSUPPORTED("지원되지 않는 토큰입니다.", UNAUTHORIZED),
+    AUTH_JWT_UNPRIVILEGED("권한이 없는 토큰입니다.", FORBIDDEN),
+
+  ;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+}
