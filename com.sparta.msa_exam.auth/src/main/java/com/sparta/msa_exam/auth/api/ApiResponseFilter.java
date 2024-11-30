c5d04acdb866d5b3605cc3fca22f5031fa0046a0
sohyuneeee <sohyuney@naver.com>
2024-12-01 02:03:54 +0900

    refactor: ApiResponseFilter OncePerRequestFilter 으로 상속변경

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/api/ApiResponseFilter.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/api/ApiResponseFilter.java
@@ -1,25 +1,27 @@
 package com.sparta.msa_exam.auth.api;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
-public class ApiResponseFilter implements Filter {
+public class ApiResponseFilter extends OncePerRequestFilter {
 
     @Value("${server.port}")
     private String serverPort;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.setHeader("Server-Port", serverPort);
-        filterChain.doFilter(servletRequest, servletResponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Server-Port", serverPort);
+        log.info("Server port: {}", response.getHeader("Server-Port"));
+        filterChain.doFilter(request, response);
     }
 
 }
