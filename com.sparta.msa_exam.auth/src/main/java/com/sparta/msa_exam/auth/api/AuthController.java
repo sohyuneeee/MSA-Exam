f648d8f70e8e77850d813c130747663131ed0d2c
sohyuneeee <sohyuney@naver.com>
2024-12-01 01:45:10 +0900

    refactor: AuthController 리팩토링

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/api/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/api/AuthController.java
@@ -3,7 +3,6 @@ package com.sparta.msa_exam.auth.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.msa_exam.auth.application.AuthService;
 import com.sparta.msa_exam.auth.infra.KakaoService;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +18,9 @@ public class AuthController {
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
     @GetMapping("/signUp/kakao")
-    public ResponseEntity<Void> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public ResponseEntity<Void> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
         String token = kakaoService.kakaoLogin(code);
-        response.addHeader(AUTHORIZATION_HEADER, token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().header(AUTHORIZATION_HEADER, token).build();
     }
 
     @PostMapping("/signUp")
@@ -32,10 +30,9 @@ public class AuthController {
     }
 
     @GetMapping("/signIn")
-    public ResponseEntity<Void> signIn(@Valid @RequestBody UserRequestDto requestDto, HttpServletResponse response) {
+    public ResponseEntity<Void> signIn(@Valid @RequestBody UserRequestDto requestDto) {
         String token = authService.signIn(requestDto);
-        response.addHeader(AUTHORIZATION_HEADER, token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().header(AUTHORIZATION_HEADER, token).build();
     }
 
 }
