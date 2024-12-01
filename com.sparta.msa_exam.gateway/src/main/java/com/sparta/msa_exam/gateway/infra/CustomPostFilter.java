f2b8186e896137be4b95a7e1781245b5bd8141cd
sohyuneeee <sohyuney@naver.com>
2024-12-01 14:14:33 +0900

    refactor: 코드 리팩토링 상수 설정 및 메소드 분리

--- com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/infra/CustomPostFilter.java
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/infra/CustomPostFilter.java
@@ -1,8 +1,6 @@
 package com.sparta.msa_exam.gateway.infra;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
@@ -13,38 +11,56 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class CustomPostFilter implements GlobalFilter, Ordered {
 
+    private static final String START_TIME_ATTR = "startTime";
+    private static final String TRACE_ID_ATTR = "traceId";
+    private static final String GATEWAY_REQUEST_URL_ATTR = ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+    private static final String SERVER_PORT_HEADER = "Server-Port";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            Long startTime = exchange.getAttribute("startTime");
-            String traceId = exchange.getAttribute("traceId");
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> handlePostFilter(exchange)));
+    }
 
-            if (startTime != null && traceId != null) {
-                Long duration = System.currentTimeMillis() - startTime;
+    private void handlePostFilter(ServerWebExchange exchange) {
+        String traceId = exchange.getAttribute(TRACE_ID_ATTR);
+        if(traceId == null) {
+            traceId = UUID.randomUUID().toString();
+        }
+        logResponse(exchange, traceId);
+        addServerPortHeader(exchange, traceId);
+    }
 
-                ServerHttpResponse response = exchange.getResponse();
-                Integer statusCode = response.getStatusCode() != null ? response.getStatusCode().value() :  - 1;
-                log.info("[{}] Outgoing Response: StatusCode = {}, Duration = {}ms", traceId, statusCode, duration);
+    private void logResponse(ServerWebExchange exchange, String traceId) {
+        Long startTime = exchange.getAttribute(START_TIME_ATTR);
+        if(startTime == null) {
+            startTime = System.currentTimeMillis();
+        }
 
-                URI routeUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
-                if (routeUri != null && routeUri.getPort() != - 1) {
-                    Integer port = routeUri.getPort();
-                    response.getHeaders().add("Server-Port", String.valueOf(port));
-                }
-                log.info("[{}] Server-Port = {}", traceId, response.getHeaders().getFirst("Server-Port"));
-            }
+        Long duration = System.currentTimeMillis() - startTime;
+        ServerHttpResponse response = exchange.getResponse();
+        Integer statusCode = response.getStatusCode() != null ? response.getStatusCode().value() :  - 1;
+        log.info("[{}] Outgoing Response: StatusCode = {}, Duration = {}ms", traceId, statusCode, duration);
+    }
 
-        }));
+    private void addServerPortHeader(ServerWebExchange exchange, String traceId) {
+        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        if (routeUri != null && routeUri.getPort() != - 1) {
+            Integer port = routeUri.getPort();
+            ServerHttpResponse response = exchange.getResponse();
+            response.getHeaders().add(SERVER_PORT_HEADER, String.valueOf(port));
+            log.info("[{}] Server-Port = {}", traceId, response.getHeaders().getFirst(SERVER_PORT_HEADER));
+        }
     }
 
     @Override
     public int getOrder() {
         return LOWEST_PRECEDENCE;
     }
-}
 
+}
--- com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/infra/CustomPreFilter.java
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/infra/CustomPreFilter.java
@@ -1,8 +1,6 @@
 package com.sparta.msa_exam.gateway.infra;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -17,17 +15,21 @@ import java.util.UUID;
 @Component
 public class CustomPreFilter implements GlobalFilter, Ordered {
 
+    private static final String START_TIME_ATTR = "startTime";
+    private static final String REQUEST_ID_ATTR = "X-Request-Id";
+    private static final String TRACE_ID_ATTR = "traceId";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         Long startTime = System.currentTimeMillis();
-        exchange.getAttributes().put("startTime", startTime);
+        exchange.getAttributes().put(START_TIME_ATTR, startTime);
 
-        String traceId = request.getHeaders().getFirst("X-Request-Id");
+        String traceId = request.getHeaders().getFirst(REQUEST_ID_ATTR);
         if(traceId == null || traceId.isEmpty()) {
             traceId = UUID.randomUUID().toString();
         }
-        exchange.getAttributes().put("traceId", traceId);
+        exchange.getAttributes().put(TRACE_ID_ATTR, traceId);
 
         log.info("[{}] Incoming Request: Method = {}, path = {}", traceId, request.getMethod(), request.getURI());
 
